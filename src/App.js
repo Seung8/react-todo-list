f0a5a0262722670a59d21e6683f94d667d88eb2d
Seungyun Lee <lsy0914@naver.com>
2020-10-03 22:49:37 +0900

    - TodoList UI 코드 작성 완료

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createGlobalStyle } from "styled-components";
 import TodoHead from "./components/TodoHead";
 import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 import TodoTemplate from "./components/TodoTemplate";
 
 // 공통 스타일 정의
@@ -18,6 +19,7 @@ function App() {
       <TodoTemplate>
         <TodoHead />
         <TodoList />
+        <TodoCreate />
       </TodoTemplate>
     </>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   /* flex: 1 => display: flex인 경우 차지할 수 있는 모든 영역을 차지 */
@@ -7,12 +8,18 @@ const TodoListBlock = styled.div`
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: lightgray;
 `;
 
 // 할일 목록 컴포넌트
 function TodoList() {
-  return <TodoListBlock>Todo List</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기1" done={true}></TodoItem>
+      <TodoItem text="프로젝트 생성하기2" done={true}></TodoItem>
+      <TodoItem text="프로젝트 생성하기3" done={false}></TodoItem>
+      <TodoItem text="프로젝트 생성하기4" done={false}></TodoItem>
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
